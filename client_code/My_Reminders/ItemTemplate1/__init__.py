94f942b2a66a8dd57542395dd5eae6f4f70d5bcb
likeeyong17@gmail.com <likeeyong17@gmail.com>
2024-07-02 01:18:09 +0000

    Created Add_Reminders, ServerModule1 and My_Reminders.ItemTemplate1; Edited settings and My_Reminders

--- client_code/My_Reminders/ItemTemplate1/__init__.py
+++ client_code/My_Reminders/ItemTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import My_RemindersTemplate
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.tables as tables
@@ -6,13 +6,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class My_Reminders(My_RemindersTemplate):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def add_reminder_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    pass
